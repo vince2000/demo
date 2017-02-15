fc98d5334e1e1284293e18de93ddd5ed11e490cc
dll <893589724@qq.com>
2017-02-15 15:31:15 +0800

    无聊修改

--- demo.go
+++ demo.go
@@ -1,6 +1,17 @@
 package main
 
+
+
 func main() {
  //dasdsadsadsadsadsa
+	/*list:=make([]string,0)
+	list=append(list,"hahha")
+	list=append(list,"qw")
+	list=append(list,"as")
+	list=append(list,"ds")
+	list=append(list,"cxz")
+	for _,r:=range list  {
+		fmt.Println(r)
+	}*/
 }
 //非常有意思
