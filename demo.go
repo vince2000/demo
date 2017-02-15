6a4aa2f3cbe00c2234bc1ae0534e86b3fd57627a
dll <893589724@qq.com>
2017-02-15 15:38:44 +0800

    啊

--- demo.go
+++ demo.go
@@ -1,10 +1,13 @@
 package main
 
-
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
  //dasdsadsadsadsadsa
-	/*list:=make([]string,0)
+	list:=make([]string,0)
 	list=append(list,"hahha")
 	list=append(list,"qw")
 	list=append(list,"as")
@@ -12,6 +15,11 @@ func main() {
 	list=append(list,"cxz")
 	for _,r:=range list  {
 		fmt.Println(r)
-	}*/
+	}
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
 //非常有意思
