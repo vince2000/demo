6d98d5b6d97336556b7d87e688beaf1174f9cfb0
dll <893589724@qq.com>
2017-02-17 15:14:11 +0800

    测试

--- demo1.go
+++ demo1.go
@@ -3,7 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	show(4)
+	//show(4)
+	/*a,b:=fanhui(11,13)
+	fmt.Println(b)
+	fmt.Println(a)*/
+	//liang(fanhui,23,78))
+	/*a,_:=fanhui(16,14)
+	fmt.Println(a)*/
+	map1:=map[string]string{}
+	map1["ka"]="ka"
+	map1["ka1"]="ka1"
+	map1["ka2"]="ka2"
+	map1["ka3"]="ka3"
+	for  r:=range  map1{
+		fmt.Println(r,map1[r])
+	}
+	var  b int
+	var  a *int
+	b=10;
+	a=&b
+	fmt.Println(b,a)
 }
 func show(a int){
 	if a>10{
@@ -14,3 +33,21 @@ func show(a int){
 		fmt.Println("zuixiao")
 	}
 }//weishane
+func fanhui(a int,b int) (int,string){
+	if a>=b {
+		a1:=[2]int{}
+		a1[0]=11
+		a1[1]=12
+		for _,r:=range a1{
+			fmt.Println(r)
+		}
+		return a,"zhenque"
+	}else {
+		return b,"cuowu"
+	}
+}
+/*
+func liang(fanhui func()){
+       a,_:=fanhui(23,78)
+	fmt.Println(a)
+}*/
